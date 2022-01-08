a8ee501441c978b896516aa4d814899e923cbb80
engalar <engalar@gmail.com>
2022-01-08 19:21:21 +0800

    demo

--- src/FlowChart.tsx
+++ src/FlowChart.tsx
@@ -9,6 +9,21 @@ import { Observer } from "mobx-react";
 import { Store } from "./store";
 import { FlowChartComponent } from "./components/FlowChartComponent";
 
+const parseStyle = (style = ""): { [key: string]: string } => {
+    try {
+        return style.split(";").reduce<{ [key: string]: string }>((styleObject, line) => {
+            const pair = line.split(":");
+            if (pair.length === 2) {
+                const name = pair[0].trim().replace(/(-.)/g, match => match[1].toUpperCase());
+                styleObject[name] = pair[1].trim();
+            }
+            return styleObject;
+        }, {});
+    } catch (_) {
+        return {};
+    }
+};
+
 
 export default function FlowChart(props: FlowChartContainerProps) {
     console.log(props);
@@ -16,6 +31,8 @@ export default function FlowChart(props: FlowChartContainerProps) {
     const [store] = useState(new Store());
 
     return <Observer>{() => (
-        <FlowChartComponent store={store} />
+        <div className="mxcn-resize-wrapper" style={parseStyle(props.style)}>
+            <FlowChartComponent store={store} />
+        </div>
     )}</Observer>;
 }
--- src/components/FlowChartComponent.tsx
+++ src/components/FlowChartComponent.tsx
@@ -1,22 +1,47 @@
-import { createElement } from "react";
+import { createElement, useEffect, useRef } from "react";
 import { Observer } from "mobx-react";
 import { Store } from "../store";
 
-import { Cascader as C } from 'antd';
+import { Graph } from "@antv/x6";
 export interface FlowChartComponentProps {
     store: Store;
 }
 
 
-const onChange = (value: any, selectedOptions: any) => {
-    console.log(value, selectedOptions);
-};
-
 export function FlowChartComponent(props: FlowChartComponentProps) {
     console.log(props);
+    const graphRef = useRef<any>();
 
+    useEffect(() => {
+        const graph: Graph = new Graph({
+            container: graphRef.current,
+            connecting: {
+                router: 'orth',
+            },
+            autoResize: true,
+            panning: false,
+            interacting: {
+                edgeMovable: false,
+                nodeMovable: true,
+            },
+        });
+
+        graph.addNode({
+            shape: 'image',
+            x: 320,
+            y: 120,
+            width: 94,
+            height: 28,
+            imageUrl:
+                'https://gw.alipayobjects.com/os/s/prod/antv/assets/image/logo-with-text-73b8a.svg',
+        })
+
+        graph.addNode({
+            x: 520, y: 170
+        });
+    }, []);
     return <Observer>{() => (
-        <C options={props.store.options} loadData={props.store.load} onChange={onChange} changeOnSelect />
+        <div className="mxcn-resize" ref={graphRef}></div>
     )}</Observer>;
 }
 
